2cafd59131b086b615c793e27874e5a36875b94b
Karolina Redden <dubajkaro@gmail.com>
2022-07-06 21:20:08 +0000

    add function to display all categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,10 +1,31 @@
 class Admin::CategoriesController < ApplicationController
+
   def index
+    @categories = Category.all
   end
 
   def new
+    @category = Category.new(name: 'test')
+    pp @category
   end
 
   def create
+    @category = Category.new(product_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  
   end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
 end
