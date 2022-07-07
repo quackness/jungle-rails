7aa7bb4c6f0167c270ccca286dae026a73a21d98
Karolina Redden <dubajkaro@gmail.com>
2022-07-07 15:40:33 +0000

    add comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,6 +30,9 @@ class ApplicationController < ActionController::Base
     cookies[:cart]
   end
 
+  # if they're logged in, and save their user object to a variable called @current_user. 
+  # The helper_method line below current_user allows us to use @current_user in our view files.
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -38,5 +41,5 @@ class ApplicationController < ActionController::Base
   def authorize
     redirect_to '/login' unless current_user
   end
-  
+
 end
